4820341ba71fe5088dcf444193b3ac526054a4fa
Sushan Sunuwar <susun022@kathmandu>
2022-06-30 18:39:16 +0545

    finished home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,70 @@
+import './index.scss'
+import React, { useEffect, useState } from 'react'
+import LogoS from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['u', 's', 'h', 'a', 'n', ',']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _11`}>i</span>
+          <span className={`${letterClass} _12`}>,</span>
+          <br />
+          <div className="first-name">
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'</span>
+            <span className={`${letterClass} _15`}>m</span>
+            <img src={LogoS} alt="developer" />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={16}
+            />
+          </div>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Frontend Developer/ JavaScript Expert/ Student</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+      <Logo />
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,62 @@
+import './index.scss'
+
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => {
+  return (
+    <div>
+      <div className="nav-bar">
+        <Link className="logo" to="/">
+          <img src={LogoS} alt="logo" />
+          <img src={LogoSubtitle} alt="sushan" className="sub-logo" />
+        </Link>
+        <nav>
+          <NavLink to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} />
+          </NavLink>
+          <NavLink className="contact-link" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </NavLink>
+        </nav>
+        <ul>
+          <li>
+            <a target="_blank" rel="noreferrer" href="https://www.facebook.com">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="google.com">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="google.com">
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </li>
+          <li>
+            <a target="_blank" rel="noreferrer" href="google.com">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
