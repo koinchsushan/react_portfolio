cd81e48ab8199c9a26a388defc22db2bf078edce
Sushan Sunuwar <susun022@kathmandu>
2022-07-04 11:02:00 +0545

    finished contact and about page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,106 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Loader from 'react-loaders'
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
-  return <div>COntact</div>
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs
+      .sendForm(
+        'service_75kfxbj',
+        'template_xqg91k4',
+        refForm.current,
+        'PyGmUNMg0X_-nmxSn'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message')
+        }
+      )
+  }
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+              letterClass={letterClass}
+            />
+          </h1>
+          <p>
+            I am interested in freelance opportunities - especially ambitious or
+            large projects. However, if you have other request or question,
+            don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={refForm} onSubmit={(e) => sendEmail(e)}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Sushan Sunuwar,
+          <br />
+          Kathmandu,
+          <br />
+          Kathmandu 220000
+          <br />
+          Lalitpur
+          <br />
+          <span>koinchsushan@gmail.com</span>
+        </div>
+      </div>
+      <Loader type="ball-rotate" />
+    </>
+  )
 }
 
 export default Contact
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
+import Loader from 'react-loaders'
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -33,37 +34,41 @@ const Home = () => {
   }, [])
 
   return (
-    <div className="container home-page">
-      <div className="text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _11`}>i</span>
-          <span className={`${letterClass} _12`}>,</span>
-          <br />
-          <div className="first-name">
-            <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'</span>
-            <span className={`${letterClass} _15`}>m</span>
-            <img src={LogoS} alt="developer" />
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _11`}>i</span>
+            <span className={`${letterClass} _12`}>,</span>
+            <br />
+            <div className="first-name">
+              <span className={`${letterClass} _13`}>I</span>
+              <span className={`${letterClass} _14`}>'</span>
+              <span className={`${letterClass} _15`}>m</span>
+              <img src={LogoS} alt="developer" />
+              <AnimatedLetters
+                letterClass={letterClass}
+                strArray={nameArray}
+                idx={16}
+              />
+            </div>
             <AnimatedLetters
               letterClass={letterClass}
-              strArray={nameArray}
-              idx={16}
+              strArray={jobArray}
+              idx={22}
             />
-          </div>
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={jobArray}
-            idx={22}
-          />
-        </h1>
-        <h2>Frontend Developer/ JavaScript Expert/ Student</h2>
-        <Link to="/contact" className="flat-button">
-          CONTACT ME
-        </Link>
+          </h1>
+          <h2>Frontend Developer/ JavaScript Expert/ Student</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
       </div>
-      <Logo />
-    </div>
+
+      <Loader type="ball-rotate" />
+    </>
   )
 }
 
